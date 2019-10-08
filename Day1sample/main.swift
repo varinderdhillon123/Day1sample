2e9994d505998804a1d226012644539649c2f4c4
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-10-07 20:20:06 -0400

    Initial Commit

---
+++ Day1sample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day1sample
+//
+//  Created by MacStudent on 2019-10-07.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
